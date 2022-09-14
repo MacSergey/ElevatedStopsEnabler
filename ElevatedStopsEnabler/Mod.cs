8f6aa27be0c54bae17941141d54749fd9d8bd563
MacSergey <macsergey@me.com>
2022-09-14 22:23:06 +0300

    workshop description

--- ElevatedStopsEnabler/Mod.cs
+++ ElevatedStopsEnabler/Mod.cs
@@ -5,7 +5,7 @@ namespace ElevatedStopsEnabler
 {
     public class Mod : IUserMod
     {
-        public string Name => "Elevated Stops Enabler";
+        public string Name => "Elevated Stops Enabler [Plazas&Promenades fix]";
 
         public string Description => "Allows to place transport stops on elevated versions of roads";
         
